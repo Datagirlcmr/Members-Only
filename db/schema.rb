22ee459806502c49c204e2fd0deeef7c42bae3ac
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-15 10:07:05 -0600

    wrote the database and model validations for name, email and password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200115152956) do
+ActiveRecord::Schema.define(version: 20200115160425) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20200115152956) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
